c4f3e1367f3278ba58f8de1ff193cc99021eefa3
Julien Malik <julien.malik@paraiso.me>
2016-06-16 11:44:16 +0200

    Refactoring to reduce number of loc

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -1,50 +1,47 @@
 class NotifierHook < Redmine::Hook::Listener
-  
-#TODO: it is plans to rename hooks in upstream
+
   def controller_issues_new_after_save(context={})
-    redmine_url = "#{Setting[:protocol]}://#{Setting[:host_name]}"
-    issue = context[:issue]
-    message = "[redmine/#{issue.project}] @#{issue.author.name} created issue ##{issue.id} #{issue.subject} #{redmine_url}/issues/#{issue.id}"
-    deliver message
+    deliver(make_msg(context[:issue], context[:issue].author.name, "created")) unless !validate_settings?
   end
-  
+
   def controller_issues_edit_after_save(context={})
-    redmine_url = "#{Setting[:protocol]}://#{Setting[:host_name]}"
-    issue = context[:issue]
-    journal = context[:journal]
-    message = "[redmine/#{issue.project}] @#{journal.user.name} updated issue ##{issue.id} #{issue.subject} #{redmine_url}/issues/#{issue.id}"
-    deliver message
+    deliver(make_msg(context[:issue], context[:journal].user.name, "updated")) unless !validate_settings?
   end
 
   private
 
+  def settings
+    @settings ||= Setting.plugin_redmine_xmpp_muc_notifications
+  end
+
+  def validate_settings?
+    settings["jid"].present? && settings["jidpassword"].present? && settings["muc_room"].present?  && settings["muc_server"].present?  && settings["nickname"].present?
+  end
+
+  def make_msg(issue, author, action, description="")
+    redmine_url = "#{Setting[:protocol]}://#{Setting[:host_name]}"
+    message = "[redmine/#{issue.project}] @#{author} #{action} issue ##{issue.id} #{issue.subject} #{redmine_url}/issues/#{issue.id}"
+    return message
+  end
+
   def deliver(message)
-    config = Setting.plugin_redmine_xmpp_muc_notifications
     begin
-      if ( config["jid"].nil? || config["jid"] == "" \
-           || config["jidpassword"].nil? || config["jidpassword"] == "" \
-           || config["muc_room"].nil? || config["muc_room"] == "" \
-           || config["muc_server"].nil? || config["muc_server"] == "" \
-           || config["nickname"].nil? || config["nickname"] == "" )
-        return
-      end
-      
       # See https://github.com/xmpp4r/xmpp4r/blob/master/data/doc/xmpp4r/examples/basic/mucsimplebot.rb
-      client = Jabber::Client.new(Jabber::JID.new(config["jid"]))
+      client = Jabber::Client.new(Jabber::JID.new(settings["jid"]))
       client.connect
-      client.auth(config["jidpassword"])
+      client.auth(settings["jidpassword"])
       
       muc = Jabber::MUC::SimpleMUCClient.new(client)
-      room_jid = "#{config["muc_room"]}@#{config["muc_server"]}/#{config["nickname"]}"
-      muc.join(room_jid)
-      
+      muc.join("#{settings["muc_room"]}@#{settings["muc_server"]}/#{settings["nickname"]}")
       muc.send Jabber::Message.new(muc.room, message)
     rescue
       ## Error connect XMPP or Error send message
       # RAILS_DEFAULT_LOGGER.error "XMPP Error: #{$!}"
+    ensure
+      client.close
+      client = nil
+      muc = nil
     end
-    client = nil
-    muc = nil
   end
   
 end
