4add27f8440e583d54ee08977e30b2d3edcdaf1d
Julien Malik <julien.malik@paraiso.me>
2016-06-16 12:34:18 +0200

    Fix syntax

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -25,7 +25,7 @@ class NotifierHook < Redmine::Hook::Listener
     html = REXML::Element::new("html").add_namespace("http://jabber.org/protocol/xhtml-im")
     html_body = REXML::Element::new("body").add_namespace("http://www.w3.org/1999/xhtml")   
     html_body_text = REXML::Text.new("[redmine/#{issue.project}] @#{author} #{action} issue <a href=\"#{redmine_url}/issues/#{issue.id}\">##{issue.id}</a> : #{issue.subject}")
-    html_body.add(t)
+    html_body.add(html_body_text)
     html.add(html_body)
 
     message = Jabber::Message.new(nil, plaintext)
